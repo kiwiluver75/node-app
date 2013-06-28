081fe27497702b6c9608aac15c4fb3564febbb5c
kiwiluver75 <kiwiluver75@gmail.com>
2013-06-28 10:37:52 -0400

    Fixed web.js

--- web.js
+++ web.js
@@ -1,31 +1,33 @@
-var express = require("express")
-	, stylus = require("stylus")
-	, nib = require("nib")
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
 
-var app = express();
 
-function compile(str, path)
-{
-	return stylus(str)
-		.set('filename', path)
-		.use(nib())
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
 }
-app.set('views',__dirname + '/views')
+
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
 app.use(stylus.middleware(
-	{ src:__dirname + '/public'
-	, compile:compile
-	}
+  { src: __dirname + '/public'
+  , compile: compile
+  }
 ))
-app.get('/', function(req, res) {
-	res.render('index',
-	{title : 'Home' }
-)
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
 })
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-})
-
+app.listen(5000)

3ab6353e1d9f02524d945cd23dd2d6388f6ce877
kiwiluver75 <kiwiluver75@gmail.com>
2013-06-28 09:50:59 -0400

    next

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
-var express = require("express");
+var express = require("express")
+	, stylus = require("stylus")
+	, nib = require("nib")
+
 var app = express();
-app.use(express.logger());
 
-app.get("/", function(request, response) {
-	response.send('Hello World! Puneeth here');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});
+function compile(str, path)
+{
+	return stylus(str)
+		.set('filename', path)
+		.use(nib())
+}
+app.set('views',__dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+	{ src:__dirname + '/public'
+	, compile:compile
+	}
+))
+app.get('/', function(req, res) {
+	res.end("testing")
+})
+app.listen(5000)
